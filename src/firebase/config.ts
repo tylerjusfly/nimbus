aa18032a77ce62b2226f1b991dee56aa2dc6170f
Tyler Momoh <tylerjusfly1@gmail.com>
2025-11-14 13:25:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-459386887-12124",
+  "appId": "1:422982510322:web:ac5186273f31731a43926b",
+  "apiKey": "AIzaSyCZTgRxXx7H3seIs7iaASox764WQGq7x38",
+  "authDomain": "studio-459386887-12124.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "422982510322"
+};
